64dd3f368b5a2d7608d1401c5b29874010cfa756
TineJereb <tine.jereb@student.um.si>
2024-04-14 16:22:17 +0200

    testiranje

--- zapakiraj_27111.sh
+++ zapakiraj_27111.sh
@@ -4,5 +4,8 @@
 sudo docker build . -t $DOCKER_USERNAME/$REPONAME:latest
 
 # Potiskanje slike v DockerHub
-sudo docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD docker.io
-sudo docker push $DOCKER_USERNAME/$REPONAME:latest
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD docker.io
+
+echo "docker build . --file Dockerfile --tag $DOCKER_USERNAME/$REPO_NAME:latest"
+docker build . --file Dockerfile --tag $DOCKER_USERNAME/$REPO_NAME:latest
+
