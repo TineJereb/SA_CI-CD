e3d4164113574d3518f56f8a303719d9d11af47c
TineJereb <tine.jereb@student.um.si>
2024-04-14 17:33:18 +0200

    testiranje

--- zazeni_27111.sh
+++ zazeni_27111.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD docker.io
+#docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD docker.io
 
 # Pridobivanje slike iz DockerHub
-sudo docker pull $DOCKER_USERNAME/$REPO_NAME:latest
+#sudo docker pull $DOCKER_USERNAME/$REPO_NAME:latest
 
 # Zagon kontejnerja
-sudo docker start test
+#sudo docker start test
