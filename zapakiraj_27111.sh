22283010f086928372a1d2ae16324ff223862007
TineJereb <tine.jereb@student.um.si>
2024-04-14 16:34:28 +0200

    testiranje

--- zapakiraj_27111.sh
+++ zapakiraj_27111.sh
@@ -8,4 +8,5 @@ docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD docker.io
 
 echo "docker build . --file Dockerfile --tag $DOCKER_USERNAME/$REPO_NAME:latest"
 docker build . --file Dockerfile --tag $DOCKER_USERNAME/$REPO_NAME:latest
+docker push $DOCKER_USERNAME/$REPO_NAME:latest
 
