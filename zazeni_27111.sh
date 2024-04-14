2576cc8789d17406aa171133a9c5c2c23a4f621d
TineJereb <tine.jereb@student.um.si>
2024-04-14 17:05:42 +0200

    testiranje

--- zazeni_27111.sh
+++ zazeni_27111.sh
@@ -4,5 +4,4 @@
 sudo docker pull $DOCKER_USERNAME/$REPO_NAME:latest
 
 # Zagon kontejnerja
-#sudo docker run --name test -v `pwd`/share:/mnt/share $DOCKER_USERNAME/$REPO_NAME:latest
 sudo docker start test
